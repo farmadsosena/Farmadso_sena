9b799aef6eaea0c191e516ca1212466d779df185
isaias <isaiasadso2023@gmail.com>
2023-10-18 23:01:27 -0500

    funciona filtro de historias

--- assets/js/menuNavConfig.js
+++ assets/js/menuNavConfig.js
@@ -1,51 +1,73 @@
- 
+window.onload = function () {
+    var currentState = sessionStorage.getItem('currentState');
 
-    // Recuperar el estado actual al cargar la página
-    window.onload = function () {
-        var currentState = sessionStorage.getItem('currentState');
-        if (currentState === 'notifications') {
-            showNotifications();
-        } else if (currentState === 'tasks') {
-            showTasks();
-        } else if (currentState === 'nuevoContenido1') {
-            showNuevoContenido1();
-        } else if (currentState === 'nuevoContenido2') {
-            showNuevoContenido2();
-        }
-    };
-
-    function showNotifications() {
-        document.getElementById('notificationsSection').style.display = 'flex';
-        document.getElementById('tasksSection').style.display = 'none';
-        document.getElementById('nuevoContenido1').style.display = 'none';
-        document.getElementById('nuevoContenido2').style.display = 'none';
-        // Guardar el estado actual
-        sessionStorage.setItem('currentState', 'notifications');
+    // Verificar si el estado almacenado en la sesión es válido
+    if (!currentState || !['notifications', 'tasks', 'nuevoContenido1', 'nuevoContenido2'].includes(currentState)) {
+        currentState = 'notifications';
     }
 
-    function showTasks() {
-        document.getElementById('notificationsSection').style.display = 'none';
-        document.getElementById('tasksSection').style.display = 'flex';
-        document.getElementById('nuevoContenido1').style.display = 'none';
-        document.getElementById('nuevoContenido2').style.display = 'none';
-        // Guardar el estado actual
-        sessionStorage.setItem('currentState', 'tasks');
+    // Llamar a la función correspondiente según el estado actual
+    if (currentState === 'notifications') {
+        showNotifications();
+    } else if (currentState === 'tasks') {
+        showTasks();
+    } else if (currentState === 'nuevoContenido1') {
+        showNuevoContenido1();
+    } else if (currentState === 'nuevoContenido2') {
+        showNuevoContenido2();
     }
+};
 
-    function showNuevoContenido1() {
-        document.getElementById('notificationsSection').style.display = 'none';
-        document.getElementById('tasksSection').style.display = 'none';
-        document.getElementById('nuevoContenido1').style.display = 'flex';
-        document.getElementById('nuevoContenido2').style.display = 'none';
-        // Guardar el estado actual
-        sessionStorage.setItem('currentState', 'nuevoContenido1');
-    }
+var tareas = document.querySelector(".tareas");
+var notificacion = document.querySelector(".notificacion");
+var historial = document.querySelector(".historial");
+var config = document.querySelector(".config");
 
-    function showNuevoContenido2() {
-        document.getElementById('notificationsSection').style.display = 'none';
-        document.getElementById('tasksSection').style.display = 'none';
-        document.getElementById('nuevoContenido1').style.display = 'none';
-        document.getElementById('nuevoContenido2').style.display = 'flex';
-        // Guardar el estado actual
-        sessionStorage.setItem('currentState', 'nuevoContenido2');
-    }
+function showNotifications() {
+    hideAllSections();
+    document.getElementById('notificationsSection').style.display = 'flex';
+    notificacion.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'notifications');
+}
+
+function showTasks() {
+    hideAllSections();
+    document.getElementById('tasksSection').style.display = 'flex';
+    tareas.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'tasks');
+}
+
+function showNuevoContenido1() {
+    hideAllSections();
+    document.getElementById('nuevoContenido1').style.display = 'flex';
+    config.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'nuevoContenido1');
+}
+
+function showNuevoContenido2() {
+    hideAllSections();
+    document.getElementById('nuevoContenido2').style.display = 'flex';
+    historial.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'nuevoContenido2');
+}
+
+function hideAllSections() {
+    document.getElementById('notificationsSection').style.display = 'none';
+    document.getElementById('tasksSection').style.display = 'none';
+    document.getElementById('nuevoContenido1').style.display = 'none';
+    document.getElementById('nuevoContenido2').style.display = 'none';
+
+    // Remover la clase azulClaro de todos los elementos
+    tareas.classList.remove("azulClaro");
+    notificacion.classList.remove("azulClaro");
+    config.classList.remove("azulClaro");
+    historial.classList.remove("azulClaro");
+}
+
+// Solo configurar el estado predeterminado cuando no hay un estado almacenado en la sesión
+if (!sessionStorage.getItem('currentState')) {
+    hideAllSections();
+    document.getElementById('notificationsSection').style.display = 'flex';
+    notificacion.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'notifications');
+}
