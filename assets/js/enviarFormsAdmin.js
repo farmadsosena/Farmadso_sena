9930ba96797027ca670179957f960f896979c2b6
yohany_06 <yohsantanilla@misena.edu.co>
2023-10-19 20:34:33 -0500

    version 1.0 formulario crear

---
+++ assets/js/enviarFormsAdmin.js
@@ -0,0 +1,27 @@
+// Funcion para enviar formularios de agregar
+
+function sendForm(event, formulario, ruta) {
+ 
+    event.preventDefault();
+    const MedicineForm = document.getElementById(formulario);
+    info = new FormData(MedicineForm);
+    fetch(ruta, {
+        method: 'POST',
+        body: info
+    }).then((response) => response.json())
+        .then(data => {
+            if(data.status === true){
+                toastr.success(data.message);
+                formulario.reset();
+            }else if(data.status === null){
+                toastr.warning(data.message);
+            }else if(data.status === 'error'){
+                toastr.error(data.message);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+}
+// Enviar form
