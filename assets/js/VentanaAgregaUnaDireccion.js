6e33f7e69f5065a260325fc35a78d886b5a3b3bc
Diego hoyos <diegohlinares2004@gmail.com>
2023-10-20 16:45:11 -0500

    Modificaciones al index y se agrega el modelo de la configuacion en html

---
+++ assets/js/VentanaAgregaUnaDireccion.js
@@ -0,0 +1,18 @@
+// Abrir y cerrar la ventana modal
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('openModalBtn');
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+span.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
