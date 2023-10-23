f7ed893d81d4edd1d870e72a745b1b9041af5f3a
Danndres <danndres9@gmail.com>
2023-10-23 09:29:06 -0500

    Solucionados los errores presentados el dia de hoy

--- assets/js/modal2G.js
+++ assets/js/modal2G.js
@@ -9,11 +9,3 @@ function cerrarG() {
     var modal = document.getElementById("miModal");
     modal.style.display = "none";
 }
-
-  // Event listener para cerrar la ventana modal haciendo clic en cualquier parte fuera del contenido
-window.onclick = function(event) {
-    var modal = document.getElementById("miModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
