8d950c5339135408add33b552a5cb9d57c139480
Nicolas Caicedo <caicedonicolas2002@gmail.com>
2023-10-23 06:21:54 -0500

    cargue de menu tres puntos

--- assets/js/menu_card.js
+++ assets/js/menu_card.js
@@ -1,16 +1,23 @@
-// Obtén una referencia al contenedor y al botón
-const openMenu = document.querySelector('.open_menu');
-const toggleButton = document.querySelector('.menu_card');
+// Obtén todas las referencias a los contenedores y botones
+const openMenus = document.querySelectorAll('.open_menu');
+const toggleButtons = document.querySelectorAll('.menu_card');
 
-// Agrega un manejador de eventos al botón de abrir y cerrar
-openMenu.addEventListener('click', toggleDisplay);
-toggleButton.addEventListener('click', toggleDisplay);
+// Agrega un manejador de eventos a cada botón de abrir y cerrar
+for (let i = 0; i < openMenus.length; i++) {
+  openMenus[i].addEventListener('click', function() {
+    toggleDisplay(i);
+  });
+  
+  toggleButtons[i].addEventListener('click', function() {
+    toggleDisplay(i);
+  });
+}
 
-function toggleDisplay() {
-  // Alterna la clase "open" en el contenedor y cambia su visibilidad
-  if (toggleButton.style.display === 'flex') {
-    toggleButton.style.display = 'none';
+function toggleDisplay(index) {
+  // Alterna la clase "open" en el botón y cambia su visibilidad
+  if (toggleButtons[index].style.display === 'flex') {
+    toggleButtons[index].style.display = 'none';
   } else {
-    toggleButton.style.display = 'flex';
+    toggleButtons[index].style.display = 'flex';
   }
 }
