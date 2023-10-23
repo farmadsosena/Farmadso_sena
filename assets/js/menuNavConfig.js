ff57e00022ea6573e89c6cfac9e987d4a8507c14
isaias <isaiasadso2023@gmail.com>
2023-10-23 07:54:37 -0500

    arreglamos el js

--- assets/js/menuNavConfig.js
+++ assets/js/menuNavConfig.js
@@ -2,63 +2,72 @@ window.onload = function () {
     var currentState = sessionStorage.getItem('currentState');
 
     // Verificar si el estado almacenado en la sesión es válido
-    if (!currentState || !['icono1', 'icono2', 'icono3'].includes(currentState)) {
-        currentState = 'icono1'; // Establecer el icono1 como estado predeterminado
+    if (!currentState || !['notifications', 'tasks', 'nuevoContenido1', 'nuevoContenido2'].includes(currentState)) {
+        currentState = 'notifications';
     }
 
     // Llamar a la función correspondiente según el estado actual
-    if (currentState === 'icono1') {
-        showContenido1();
-    } else if (currentState === 'icono2') {
-        showContenido2();
-    } else if (currentState === 'icono3') {
-        showContenido3();
+    if (currentState === 'notifications') {
+        showNotifications();
+    } else if (currentState === 'tasks') {
+        showTasks();
+    } else if (currentState === 'nuevoContenido1') {
+        showNuevoContenido1();
+    } else if (currentState === 'nuevoContenido2') {
+        showNuevoContenido2();
     }
 };
 
-var icono1 = document.getElementById('icono1');
-var icono2 = document.getElementById('icono2');
-var icono3 = document.getElementById('icono3');
+var tareas = document.querySelector(".tareas");
+var notificacion = document.querySelector(".notificacion");
+var historial = document.querySelector(".historial");
+var config = document.querySelector(".config");
 
-icono1.addEventListener('click', function() {
-    showContenido1();
-});
-
-icono2.addEventListener('click', function() {
-    showContenido2();
-});
-
-icono3.addEventListener('click', function() {
-    showContenido3();
-});
-
-function showContenido1() {
-    // Ocultar los otros contenidos si es necesario
-    hideOtherContenidos();
-    // Mostrar el contenido correspondiente al icono1
-    document.getElementById('contenido1').style.display = 'block';
-    sessionStorage.setItem('currentState', 'icono1');
+function showNotifications() {
+    hideAllSections();
+    document.getElementById('notificationsSection').style.display = 'flex';
+    notificacion.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'notifications');
 }
 
-function showContenido2() {
-    // Ocultar los otros contenidos si es necesario
-    hideOtherContenidos();
-    // Mostrar el contenido correspondiente al icono2
-    document.getElementById('contenido2').style.display = 'block';
-    sessionStorage.setItem('currentState', 'icono2');
+function showTasks() {
+    hideAllSections();
+    document.getElementById('tasksSection').style.display = 'flex';
+    tareas.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'tasks');
 }
 
-function showContenido3() {
-    // Ocultar los otros contenidos si es necesario
-    hideOtherContenidos();
-    // Mostrar el contenido correspondiente al icono3
-    document.getElementById('contenido3').style.display = 'block';
-    sessionStorage.setItem('currentState', 'icono3');
+function showNuevoContenido1() {
+    hideAllSections();
+    document.getElementById('nuevoContenido1').style.display = 'flex';
+    config.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'nuevoContenido1');
 }
 
-function hideOtherContenidos() {
-    // Ocultar todos los contenidos
-    document.getElementById('contenido1').style.display = 'none';
-    document.getElementById('contenido2').style.display = 'none';
-    document.getElementById('contenido3').style.display = 'none';
+function showNuevoContenido2() {
+    hideAllSections();
+    document.getElementById('nuevoContenido2').style.display = 'flex';
+    historial.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'nuevoContenido2');
+}
+
+function hideAllSections() {
+    document.getElementById('notificationsSection').style.display = 'none';
+    document.getElementById('tasksSection').style.display = 'none';
+    document.getElementById('nuevoContenido1').style.display = 'none';
+    document.getElementById('nuevoContenido2').style.display = 'none';
+
+    // Remover la clase azulClaro de todos los elementos
+    tareas.classList.remove("azulClaro");
+    notificacion.classList.remove("azulClaro");
+    config.classList.remove("azulClaro");
+    historial.classList.remove("azulClaro");
+}
+
+// Solo configurar el estado predeterminado cuando no hay un estado almacenado en la sesión
+if (!sessionStorage.getItem('currentState')) {
+    hideAllSections();
+    document.getElementById('notificationsSection').style.display = 'flex';
+    notificacion.classList.add("azulClaro");
+    sessionStorage.setItem('currentState', 'notifications');
 }
--- domiciliario.php
+++ domiciliario.php
@@ -299,9 +299,7 @@
       <section class="mainDeliverySection" id="notificationsSection">
         <section class="notificationsDelivery">
 
-         <?php
-            include("php/parques.php")
-            ?>
+         
 
           <!-- <article class="orderAvailable">
             <p>001</p>
