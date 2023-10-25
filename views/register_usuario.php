7d28084956797e65f6312262cb7c02c6533fb680
sara <saraytrujillo2004@gmail.com>
2023-10-25 11:28:57 -0500

    cambios_nuevos_registro

--- views/register_usuario.php
+++ views/register_usuario.php
@@ -14,65 +14,92 @@
         <form class="form" method="post">
             <p class="title">Registro de Usuario</p>
             <p class="message">Regístrate ahora y obtén acceso completo a nuestro sistema.</p>
-            <label class="contenedor_label">
-                <span class="letra_label">Nombre</span>
-                <input class="contenedores_input" type="text" name="nombre" placeholder="" required>
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Apellido</span>
-                <input class="contenedores_input" type="text" name="apellido" placeholder="" required>
-             
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Tipo de Documento</span>
-                <select class="contendor_select" name="tipo_documento" required>
-                    <option value="1">Cédula</option>
-                    <option value="2">Tarjeta de identidad</option>
-                </select>
-              
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Número de Documento</span>
-                <input class="contenedores_input" type="text" name="documento" placeholder="" required>
-         
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Email</span>
-                <input class="contenedores_input" type="email" name="correo" placeholder="" required>
-           
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Contraseña</span>
-                <input class="contenedores_input" type="password" name="passwordusuario" placeholder="" required>
-              
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Confirmar contraseña</span>
-                <input class="contenedores_input" type="password" name="confirmar_contra" placeholder="" required>
-               
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">Teléfono</span>
-                <input class="contenedores_input" type="number" name="telefono" placeholder="" required>
-                
-            </label>
-            <label  class="contenedor_label">
-                <span class="letra_label">EPS</span>
-                <select class="contendor_select" name="IdEps" required>
-                    <option value="1">EPS 1</option>
-                    <option value="2">EPS 2</option>
-                    <option value="3">EPS 3</option>
-                </select>
-           
-            </label>
+            <section class="parte1-formulario">
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Nombre</span>
+                        <input class="contenedores_input" type="text" name="nombre" placeholder="" required>
+                    </label>
+                </div>
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Apellido</span>
+                        <input class="contenedores_input" type="text" name="apellido" placeholder="" required>
+                    </label>
+                </div>
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Tipo de Documento</span>
+                        <select class="contendor_select" name="tipo_documento" required>
+                            <option value="1">Cédula</option>
+                            <option value="2">Tarjeta de identidad</option>
+                        </select>
+                    </label>
+                </div>
+            </section>
+
+            <section class="parte1-formulario">
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Número de Documento</span>
+                        <input class="contenedores_input" type="text" name="documento" placeholder="" required>
+
+                    </label>
+                </div>
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Email</span>
+                        <input class="contenedores_input" type="email" name="correo" placeholder="" required>
+
+                    </label>
+                </div>
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Contraseña</span>
+                        <input class="contenedores_input" type="password" name="passwordusuario" placeholder=""
+                            required>
+
+                    </label>
+                </div>
+            </section>
+            <section class="parte1-formulario">
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Confirmar contraseña</span>
+                        <input class="contenedores_input" type="password" name="confirmar_contra" placeholder=""
+                            required>
+
+                    </label>
+                </div>
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">Teléfono</span>
+                        <input class="contenedores_input" type="number" name="telefono" placeholder="" required>
+
+                    </label>
+                </div>
+                <div class="contenedoresparte1">
+                    <label class="contenedor_label">
+                        <span class="letra_label">EPS</span>
+                        <select class="contendor_select" name="IdEps" required>
+                            <option value="1">EPS 1</option>
+                            <option value="2">EPS 2</option>
+                            <option value="3">EPS 3</option>
+                        </select>
+
+                    </label>
+                </div>
+            </section>
             <button class="submit" type="submit">Registrar</button>
             <p class="signin">¿Ya tienes una cuenta? <a href="login.php">Login</a></p>
         </form>
     </section>
+    </section>
 
-    <?php
-      include("../controllers/procesar_formulario.php");
-    ?>
+ 
+<?php
+include("../controllers/procesar_formulario.php");
+?>
 </body>
 
 </html>
