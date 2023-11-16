7d3cb8c222953af04d8a381015eef91f08d75f88
felipe <cristianmartinez4002@gmail.com>
2023-11-16 10:42:19 -0500

    Envio vacio

--- assets/js/modalCompras.js
+++
@@ -1,9 +0,0 @@
-function detalleCompra() {
-    var modal = document.getElementById("modalDetalle");
-    modal.style.display = "block";
-}
-
-function cerrarModal() {
-    var modal = document.getElementById("modalDetalle");
-    modal.style.display = "none";
-}
