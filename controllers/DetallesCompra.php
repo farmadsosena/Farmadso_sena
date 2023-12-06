9659bec7aea19f0ceef70da5976626ef083c9204
vanessa <dellyvanessacruz007@gmail.com>
2023-12-05 21:02:26 -0500

    actualizacion del codigo de mis compras con otra version de base de datos

--- controllers/DetallesCompra.php
+++ controllers/DetallesCompra.php
@@ -1,46 +1,50 @@
 <?php
-    // Verifica si se ha proporcionado el parámetro idcompra en la solicitud GET
+   // Verifica si se ha proporcionado el parámetro idcompra en la solicitud GET
     if (isset($_GET['idcompra'])) {
         $idcompra = $_GET['idcompra'];
 
         // Realiza la consulta a la base de datos para obtener los detalles de la compra
         require_once '../config/Conexion.php';
 
-        // Consulta para obtener los detalles de la compra y la fecha desde la tabla detallecompra y compra
-        $sqlDetalles = "SELECT dc.id, m.nombre as nombre_medicamento, dc.cantidad, dc.preciototal, dc.idDirecciones, c.fecha as fecha_compra
+        // Consulta para obtener los detalles de la compra desde las tablas relacionadas
+        $sqlDetalles = "SELECT dc.id, m.nombre as nombre_medicamento, dc.cantidad, dc.preciototal
                         FROM detallecompra dc
                         INNER JOIN compra c ON dc.idcompra = c.idcompra
                         INNER JOIN medicamentos m ON dc.idmedicamento = m.idmedicamento
-                        WHERE dc.idcompra = $idcompra";
+                        WHERE dc.idcompra = ?";
 
-        $resultDetalles = $conexion->query($sqlDetalles);
+        // Preparar la consulta
+        $stmt = $conexion->prepare($sqlDetalles);
 
-        // Crear un array para almacenar los detalles
-        $data = array();
+        if ($stmt) {
+            // Vincular parámetro
+            $stmt->bind_param("i", $idcompra);
 
-        if ($resultDetalles->num_rows > 0) {
-            // Obtener la fecha de la compra (será la misma para todos los detalles de compra relacionados)
-            $fechaCompra = null;
-            while ($rowDetalle = $resultDetalles->fetch_assoc()) {
-                // Establecer la fecha de compra para todos los detalles de compra
-                if ($fechaCompra === null) {
-                    $fechaCompra = $rowDetalle['fecha_compra'];
+            // Ejecutar la consulta
+            $stmt->execute();
+
+            // Obtener resultados
+            $resultDetalles = $stmt->get_result();
+
+            // Crear un array para almacenar los detalles
+            $data = array();
+
+            if ($resultDetalles->num_rows > 0) {
+                while ($rowDetalle = $resultDetalles->fetch_assoc()) {
+                    $data[] = $rowDetalle;
                 }
-
-                // Formatear la cantidad y el precio total con separadores de miles
-                $rowDetalle['cantidad'] = number_format($rowDetalle['cantidad'], 0, ',', '.');
-                $rowDetalle['preciototal'] = number_format($rowDetalle['preciototal'], 0, ',', '.');
-
-
-                // Agregar la fecha de compra y el nombre del medicamento al detalle de compra
-                $rowDetalle['fecha_compra'] = $fechaCompra;
-                $data[] = $rowDetalle;
+            } else {
+                $data[] = array(
+                    'mensaje' => 'No se encontraron detalles de compra para el ID proporcionado.'
+                );
             }
-        }
 
-        // Devolver los detalles de la compra como respuesta en formato JSON
-        header('Content-Type: application/json');
-        echo json_encode($data);
+            // Devolver los detalles de la compra como respuesta en formato JSON
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        } else {
+            echo "Error en la preparación de la consulta.";
+        }
     } else {
         // Si no se proporcionó idcompra, puedes manejar el error de alguna manera
         echo "ID de compra no proporcionado.";
